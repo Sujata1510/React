e12eaa7f6ee4bc90833526c7498627aecd13696e
Sujata1510 <sujakutuwa2@gmail.com>
2024-11-23 16:06:27 +0545

    add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#273036" fillOpacity="1" d="M0,192L1440,32L1440,320L0,320Z"></path>
-      <text x="750" y="250" fill="white" fontSize={"30px"}>Developed By:
+    <div className="relative"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#273036" fillOpacity="1" d="M0,192L1440,32L1440,320L0,320Z"></path>
+      {/* <text x="750" y="250" fill="white" fontSize={"30px"}>Developed By:
 
       </text>
       <text x="750" y="300" fill="white" fontSize={"40px"}>
         Sujata Kutuwa
-      </text>
-    </svg></div>
+      </text> */}
+    </svg>
+      <div className="absolute bottom-[25%] sm:[10%] left:">
+        <p className="text-red-400">Develop By</p>
+      </div>
+
+
+    </div>
   )
 }
 
--- src/components/home_components/Info.jsx
+++ src/components/home_components/Info.jsx
@@ -4,7 +4,7 @@ const Info = () => {
   return (
     <div className='space-y-6'>
       <h1 className="text-3xl font-bold text-center ">Who am I</h1>
-      <div className="h-[400px] w-[400px] rounded-full bg-black flex justify-center items-center mx-auto border-gray-600 border-[6px]">
+      <div className="h-[400px] w-[400px] rounded-full bg-black flex justify-center items-center mx-auto border-gray-600 border-[6px] sm:h-[300px] sm:w-[210px] md:h-[300px] md:w-[300px]">
         <div className="text-white">
           <p>Name : Sujata Kutuwa</p>
           <p>Email : sujakutuwa2@gmail.com</p>
